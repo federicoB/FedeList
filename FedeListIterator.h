0a575d0228de2a884bf78fae0ca0411f191b5e6d
federicoB <federicobertani96@gmail.com>
2016-03-01 20:16:23 +0100

    added documentation to operators overloading

--- FedeListIterator.h
+++ FedeListIterator.h
@@ -45,32 +45,85 @@ public:
     /**
      * Override assignemnt operator.
      * @param right FedeListIterator&lt;ListType*&gt;& : the object to assign.
-     * @return FedeListIterator&lt;ListType*&gt;& : the object assigned
+     * @return FedeListIterator&lt;ListType*&gt;*: the object assigned
      */
     FedeListIterator<ListType>& operator=(const FedeListIterator<ListType>& right);
-
-    FedeListIterator<ListType> &operator[](const int position);
-
-    FedeListIterator<ListType>& operator*();
-
+    
+    /**
+     * Ovverride direct access operator.This browse the list until the given position is found.
+     * @param position int: the position to get .
+     * @return FedeListIterator&lt;ListType*&gt;* : a pointer to the element searched
+     */
+    FedeListIterator<ListType>& operator[](const int position);
+    
+    /**
+     * Get the node pointed by the iterator.
+     * @return FedeListIterator&lt;ListType*&gt;*: the node pointed by the iterator.
+     */
+    Node<ListType>& operator*();
+    
+    /**
+     * Compare two Iterators, if they point to the same node they are the same iterator.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool: true if the iterators are the same, false otherwise.
+     */
     bool operator==(const FedeListIterator<ListType> &right);
-
+    
+    /**
+     * Compare two iterator, if they point to different nodes they are different iterators.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool:true if the iterator are the same, false otherwise..
+     */
     bool operator!=(const FedeListIterator<ListType> &right);
-
+    
+    /**
+     * Compare two iterators and check if an iterator is pointing at a node that is behind onother.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool: true if the iterator is smaller, false otherwise.
+     */
     bool operator<(const FedeListIterator<ListType> &right);
-
+    
+   /**
+     * Compare two iterators and check if an iterator is pointing at a node that is behind onother or the same.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool: true if the iterator is smaller or equal, false otherwise.
+     */
     bool operator<=(const FedeListIterator<ListType> &right);
-
+    
+   /**
+     * Compare two iterators and check if an iterator is pointing at a node that is ahead onother.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool: true if the iterator is greater, false otherwise.
+     */
     bool operator>(const FedeListIterator<ListType> &right);
-
+    
+   /**
+     * Compare two iterators and check if an iterator is pointing at a node that is ahead onother or they are the same.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to compare
+     * @return bool: true if the iterator is greater or equal false otherwise.
+     */
     bool operator>=(const FedeListIterator<ListType> &right);
-
+    
+    /**
+     * Create a new iterator that point to the position in the list that is the sum of the position of two others iterators.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to sum
+     * @return FedeListIterator&lt;ListType*&gt;*: the new iterator.
+     */
     FedeListIterator<ListType> operator+(const FedeListIterator<ListType> &right);
-
+    
+    /**
+     * Create a new iterator that point to the position in the list that is the difference of the position of two other iterators.
+     * @param right FedeListIterator&lt;ListType*&gt;*: the iterator to subtract
+     * @return FedeListIterator&lt;ListType*&gt;*: the new iterator.
+     */
     FedeListIterator<ListType> operator-(const FedeListIterator<ListType> &right);
-
+    
+    /**
+     * Advance the iterator to a position.
+     * @return FedeListIterator&lt;ListType*&gt;*: the new iterator.
+     */
     FedeListIterator<ListType>& operator++();
-
+    
     FedeListIterator<ListType>& operator++( int );
 
     FedeListIterator<ListType>& operator--();
