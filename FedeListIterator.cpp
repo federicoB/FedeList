9e308e06ead5578dc7f786d78f9bd7ad23e67fde
federicoB <federicobertani96@gmail.com>
2016-03-02 19:00:35 +0100

    Implmented [] ++ and -- operators

--- FedeListIterator.cpp
+++ FedeListIterator.cpp
@@ -30,6 +30,106 @@ FedeListIterator<ListType>::~FedeListIterator() {
     //do nothing because pointer is automatically destroyed.
 }
 
+template <class ListType>
+FedeListIterator<ListType> &FedeListIterator<ListType>::operator[](const int position) {
+    //check if the position is ahead to the cursor
+    bool isPositionAhead = this->position < position;
+    //while the cursor position is not equal than the position
+    while (this->position != position) {
+        //if the position is ahead
+        if (isPositionAhead) {
+            *this++;
+        }
+            //else (position is before the cursor)
+        else {
+            *this--;
+        }
+    }
+}
+
+template <class ListType>
+Node<ListType> &FedeListIterator<ListType>::operator*() {
+    return *this->pointer;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator==(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator!=(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator<(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator<=(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator>(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+bool FedeListIterator<ListType>::operator>=(const FedeListIterator<ListType> &right) {
+    return false;
+}
+
+template <class ListType>
+FedeListIterator<ListType> FedeListIterator<ListType>::operator+(const FedeListIterator<ListType> &right) {
+    return FedeListIterator();
+}
+
+template <class ListType>
+FedeListIterator<ListType> FedeListIterator<ListType>::operator-(const FedeListIterator<ListType> &right) {
+    return FedeListIterator();
+}
+
+template <class ListType>
+FedeListIterator<ListType> &FedeListIterator<ListType>::operator++() {
+    FedeListIterator<ListType>& oldOne = *this;
+    //move the cursor ahead
+    this->pointer = this->pointer -> getNext();
+    //increase the cursor position
+    this->position++;
+    return oldOne;
+}
+
+template <class ListType>
+FedeListIterator<ListType> &FedeListIterator<ListType>::operator++(int i) {
+    //move the cursor ahead
+    this->pointer = this->pointer -> getNext();
+    //increase the cursor position
+    this->position++;
+    return *this;
+}
+
+template <class ListType>
+FedeListIterator<ListType> &FedeListIterator<ListType>::operator--() {
+    FedeListIterator<ListType>& oldOne = *this;
+    //move the cursor back
+    this->pointer = this->pointer -> getPrev();
+    //decrease the cursor position
+    this->position--;
+    return oldOne;
+}
+
+template <class ListType>
+FedeListIterator<ListType> &FedeListIterator<ListType>::operator--(int i) {
+    //move the cursor back
+    this->pointer = this->pointer -> getPrev();
+    //decrease the cursor position
+    this->position--;
+    return *this;
+}
+
 template<class ListType>
 FedeListIterator<ListType>& FedeListIterator<ListType>::operator=(const FedeListIterator<ListType>& right) {
         // Check for self-assignment!
